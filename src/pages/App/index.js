b1cb9b84a82b6c0ded2820d2380682e2373fdf2d
Vadaa-Hi <69497241+Vadaa-Hi@users.noreply.github.com>
2022-09-19 11:54:59 +0800

    init

---
+++ src/pages/App/index.js
@@ -0,0 +1,16 @@
+import './style.css';
+import Toolbar from '../../components/Toolbar';
+import BurgerBuilder from '../BurgerBuilder';
+
+function App() {
+  return (
+    <div>
+      <Toolbar />
+      <main className="Content">
+        <BurgerBuilder />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/BurgerBuilder/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// dotroo state hadgaldag class component-r hiiy
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <div>
+        <div>Хачиртай талх</div>
+        <div>Орцны удирдлага</div>
+      </div>
+    );
+  }
+}
+
+export default BurgerBuilder;
