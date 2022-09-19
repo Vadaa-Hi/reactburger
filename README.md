7cf4c40838e0feae13ad2a47aed319be0000af7f
Vadaa-Hi <69497241+Vadaa-Hi@users.noreply.github.com>
2022-09-19 12:43:29 +0800

    create Burger&BurgerIngredient folder

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 1. Хавтаснуудын зохион байгуулалтыг хийх
    BurgerBuilder болон Toolbar component-ийг холбож ажилуулав.
 2. CSS Module ашиглах Toolbar component ийг хэлбэржүүлэх
+3. Burger, BurgerIngrediend component-ийг холболтуудыг хийж дэлгэцэнд гаргав
--- src/pages/BurgerBuilder/index.js
+++ src/pages/BurgerBuilder/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import Burger from '../../components/Burger';
 // dotroo state hadgaldag class component-r hiiy
 class BurgerBuilder extends Component {
   render() {
     return (
       <div>
-        <div>Хачиртай талх</div>
+        <Burger />
         <div>Орцны удирдлага</div>
       </div>
     );
